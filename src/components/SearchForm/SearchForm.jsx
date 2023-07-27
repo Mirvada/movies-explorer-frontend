3afff90651d4551668df7532dad0d2f6d0921d43
Aleksandra Kolianichenko <mirvada1@yandex.ru>
2023-07-27 22:25:40 +0300

    устраняет обновление страницы после отправки формы

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import './SearchForm.css';
 import FilterCheckbox from '../FilterCheckbox/FilterCheckbox';
+import { useFormAndValidation } from '../../hooks/useFormAndValidation';
 
 const SearchForm = () => {
+  const { values, handleChange, resetForm } = useFormAndValidation();
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    resetForm();
+  };
+
   return (
-    <form className='seachForm'>
+    <form className='seachForm' onSubmit={handleSubmit}>
       <label className='seachForm__label'>
-        <input className='seachForm__input' type='search' placeholder='Фильм' />
-        <button className='seachForm__button' type='button' />
+        <input
+          className='seachForm__input'
+          type='search'
+          name='search'
+          value={values.search ?? ''}
+          onChange={handleChange}
+          placeholder='Фильм'
+        />
+        <button className='seachForm__button' type='submit' />
       </label>
       <FilterCheckbox />
     </form>
