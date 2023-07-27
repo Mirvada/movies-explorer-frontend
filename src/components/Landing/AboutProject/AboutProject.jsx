84eae109e5f1d720b6e37d19e2dbc4227f444a5f
Aleksandra Kolianichenko <mirvada1@yandex.ru>
2023-07-27 21:57:07 +0300

    исправлен бэм

--- src/components/Landing/AboutProject/AboutProject.jsx
+++ src/components/Landing/AboutProject/AboutProject.jsx
@@ -3,35 +3,35 @@ import './AboutProject.css';
 
 const AboutProject = () => {
   return (
-    <section className='aboutProject' id='project'>
-      <h2 className='aboutProject__title'>О проекте</h2>
-      <div className='aboutProject__container'>
-        <div className='aboutProject__wripper'>
-          <h3 className='aboutProject__subtitle'>
+    <section className='about-project' id='project'>
+      <h2 className='about-project__title'>О проекте</h2>
+      <div className='about-project__container'>
+        <div className='about-project__wripper'>
+          <h3 className='about-project__subtitle'>
             Дипломный проект включал 5 этапов
           </h3>
-          <p className='aboutProject__text'>
+          <p className='about-project__text'>
             Составление плана, работу над бэкендом, вёрстку, добавление
             функциональности и финальные доработки.
           </p>
         </div>
-        <div className='aboutProject__wripper'>
-          <h3 className='aboutProject__subtitle'>
+        <div className='about-project__wripper'>
+          <h3 className='about-project__subtitle'>
             На выполнение диплома ушло 5 недель
           </h3>
-          <p className='aboutProject__text'>
+          <p className='about-project__text'>
             У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было
             соблюдать, чтобы успешно защититься.
           </p>
         </div>
       </div>
-      <div className='aboutProject__progressBar'>
-        <h3 className='aboutProject__indicator aboutProject__indicator_type_complited'>
+      <div className='about-project__progressBar'>
+        <h3 className='about-project__indicator about-project__indicator_type_complited'>
           1 неделя
         </h3>
-        <h3 className='aboutProject__indicator'>4 недели</h3>
-        <p className='aboutProject__captcha'>Back-end</p>
-        <p className='aboutProject__captcha'>Front-end</p>
+        <h3 className='about-project__indicator'>4 недели</h3>
+        <p className='about-project__caption'>Back-end</p>
+        <p className='about-project__caption'>Front-end</p>
       </div>
     </section>
   );
