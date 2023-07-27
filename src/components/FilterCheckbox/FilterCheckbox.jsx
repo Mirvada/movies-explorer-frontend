532ee96786afff494f0871d75b439fbc3a54c3d0
Aleksandra Kolianichenko <mirvada1@yandex.ru>
2023-07-27 22:31:38 +0300

    исправляет checkbox

--- src/components/FilterCheckbox/FilterCheckbox.jsx
+++ src/components/FilterCheckbox/FilterCheckbox.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './FilterCheckbox.css';
 
 const FilterCheckbox = () => {
   return (
     <label className='checkbox__label'>
-      <input className='checkbox__input' type='checkbox' checked='checked' />
+      <input
+        className='checkbox__input'
+        type='checkbox'
+        defaultChecked='checked'
+      />
       <span className='checkbox__inner'>Короткометражки</span>
     </label>
   );
